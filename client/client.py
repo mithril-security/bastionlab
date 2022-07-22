23b85955b99251f55774b18774d073b2ebbccdbb
kbamponsem <kwabenaamponsem@gmail.com>
2022-07-22 18:22:41 +0200

    Fixes minimal issues with ser/deser and makes it work with client

--- client/client.py
+++ client/client.py
@@ -2,38 +2,48 @@ from dataclasses import dataclass
 from typing import Any, Iterator, List
 from torch.nn import Module
 from torch.utils.data import Dataset
-from utils import serialize_model, serialize_dataset
-from pb.remote_torch_pb2_grpc import ReferenceProtocolStub
-from pb.remote_torch_pb2 import AvailableObject, Chunk, Empty, Reference, TrainConfig
+from utils import serialize_dataset, serialize_model
+from pb.remote_torch_pb2_grpc import RemoteTorchStub
+from pb.remote_torch_pb2 import Chunk, Empty, Reference, TrainConfig
 import grpc
 
 
 @dataclass
 class Client:
-    stub: ReferenceProtocolStub
+    stub: RemoteTorchStub
 
-    def send_model(self, model: Module, description: str) -> Reference:
+    def send_model(self, model: Module, description: str, secret: bytes) -> Reference:
         if not hasattr(model, "trainable_parameters"):
             raise Exception(
                 "This model is not fully compatible with remote excecution on a BastionAI server. Consider using the @remote_module decorator.")
         if not hasattr(model, "grad_sample_parameters"):
             print("W: This model is not compatible with private optimizers, if you need DP guarantees, consider instantiating a PrivacyEngine and using the @engine.private_module(...) decorator.")
-        return self.stub.SendModel(serialize_model(model, description=description))
+        return self.stub.SendModel(serialize_model(model, description=description, secret=secret))
 
-    def send_dataset(self, dataset: Dataset, description: str) -> Reference:
-        return self.stub.SendData(serialize_dataset(dataset, description=description))
+    def send_dataset(self, dataset: Dataset, description: str, secret: bytes) -> Reference:
+
+        return self.stub.SendDataset(serialize_dataset(dataset, description=description, secret=secret))
 
     def fetch_model(self, ref: Reference) -> Iterator[Chunk]:
-        return self.stub.Fetch(ref)
+        return self.stub.FetchModule(ref)
 
-    def get_available_models(self) -> List[AvailableObject]:
-        return self.stub.GetAvailableModels(Empty())
+    def fetch_dataset(self, ref: Reference) -> Iterator[Chunk]:
+        return self.stub.FetchDataset(ref)
 
-    def get_available_datasets(self) -> List[AvailableObject]:
-        return self.stub.GetAvailableDataSets(Empty())
+    def get_available_models(self) -> List[Reference]:
+        return self.stub.AvailableModels(Empty())
+
+    def get_available_datasets(self) -> List[Reference]:
+        return self.stub.AvailableDatasets(Empty())
 
     def train(self, config: TrainConfig) -> Reference:
         return self.stub.Train(config)
+
+    def delete_dataset(self, ref: Reference) -> Empty:
+        return self.stub.DeleteDataset(ref)
+
+    def delete_module(self, ref: Reference) -> Empty:
+        return self.stub.DeleteModule(ref)
 
 
 @dataclass
@@ -44,7 +54,7 @@ class Connection:
 
     def __enter__(self) -> Client:
         self.channel = grpc.insecure_channel("localhost:50051")
-        return Client(ReferenceProtocolStub(self.channel))
+        return Client(RemoteTorchStub(self.channel))
 
     def __exit__(self, exc_type: Any, exc_value: Any, exc_traceback: Any) -> None:
         self.channel.close()
@@ -56,16 +66,23 @@ if __name__ == '__main__':
     dataset = DummyDataset()
 
     with Connection("localhost", 50051) as client:
-        model_ref = client.send_model(model, "DummyModel")
+        model_ref = client.send_model(model, "DummyModel", b"secret")
         print(f"Model ref: {model_ref}")
         # res = client.fetch_model(model_ref)
 
+        res = client.send_dataset(dataset, "DummyDataset", b'secret')
+        print(f"Dataset ref: {res}")
+
+        res = client.delete_module(
+            Reference(identifier="881379b7-0a24-4e00-9d3c-716b0b31f6b5"))
+        # print(next(res))
+
         res = client.get_available_models()
-        for model in res.available_objects: # type: ignore
-            print(f"{model.reference}, {model.description}")
+        for model in res.list:  # type: ignore
+            print(f"{model.identifier}, {model.description}")
 
-        dataset_ref = client.send_dataset(dataset, "DummyDataset")
-        print(f"Dataset ref: {dataset_ref}")
+        # dataset_ref = client.send_dataset(dataset, "DummyDataset")
+        # print(f"Dataset ref: {dataset_ref}")
 
-        client.train(TrainConfig(model=model_ref, dataset=dataset_ref, batch_size=64, epochs=1, learning_rate=1e-3))
-
+        # client.train(TrainConfig(model=model_ref, dataset=dataset_ref,
+        #              batch_size=64, epochs=1, learning_rate=1e-3))
--- client/dummy.py
+++ client/dummy.py
@@ -1,37 +1,38 @@
 import torch
-from torch.nn import Module, Linear 
+from torch.nn import Module, Linear
 from torch.nn.functional import relu
 from torch.utils.data import Dataset
-from typing import Tuple
+from typing import List, Tuple
 from private_module import PrivacyEngine
 from utils import remote_module
 
 engine = PrivacyEngine()
+
 
 class DummyDataset(Dataset):
     def __init__(self) -> None:
         super().__init__()
         self.X = torch.randn(10, 784)
         self.Y = torch.randint(0, 10, (10,))
-    
+
     def __len__(self) -> int:
         return 10
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         return (self.X[idx], self.Y[idx])
 
+
 @remote_module
 @engine.private_module(max_batch_len=64)
 class DummyModel(Module):
     def __init__(self) -> None:
         super().__init__()
-        self.fc1 = Linear(784, 1568)
-        self.fc2 = Linear(1568, 10)
+        self.fc1 = Linear(10, 20)
+        self.fc2 = Linear(20, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.view(-1, 784)
+        x = x.view(-1, 10)
         x = self.fc1(x)
         x = relu(x)
         x = self.fc2(x)
         return x
-
