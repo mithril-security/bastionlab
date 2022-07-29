b4094923faa9a7ec510ed64799e5c19273a58fd3
Lucas Bourtoule <lucas@bourtoule.com>
2022-07-29 14:54:35 +0200

    working Linear and Conv PSG layers

--- private_learning/src/lib.rs
+++ private_learning/src/lib.rs
@@ -1,4 +1,4 @@
-use tch::{TchError, TrainableCModule, COptimizer, Tensor, IValue, Kind, Device};
+use tch::{TchError, COptimizer, Tensor, Kind, nn::VarStore, IndexOp};
 
 #[cfg(test)]
 mod tests {
@@ -14,8 +14,8 @@ mod tests {
     #[test]
     fn basic_sgd() {
         let vs = VarStore::new(Device::Cpu);
-        let model = TrainableCModule::load("../client/lreg.pt", vs.root()).unwrap();
-        let parameters = Parameters::try_new(&model).unwrap();
+        let model = TrainableCModule::load("../client/tests/lreg.pt", vs.root()).unwrap();
+        let parameters = Parameters::from(vs);
         let mut optimizer = SGD::new(parameters, 0.1);
 
         let data = vec![
@@ -36,15 +36,15 @@ mod tests {
                 optimizer.step().unwrap();
             }
         }
-        let w = &vs.trainable_variables()[0];
+        let w = &optimizer.parameters.into_inner()[0];
         assert!((w - Tensor::of_slice::<f32>(&[2.])).abs().double_value(&[]) < 0.1);
     }
 
     #[test]
     fn private_sgd() {
         let vs = VarStore::new(Device::Cpu);
-        let model = TrainableCModule::load("../client/private_lreg.pt", vs.root()).unwrap();
-        let parameters = PrivateParameters::try_new(&model, 1000.0, 0.0, LossType::Mean(2)).unwrap();
+        let model = TrainableCModule::load("../client/tests/private_lreg.pt", vs.root()).unwrap();
+        let parameters = PrivateParameters::new(vs, 1.0, 0.1, LossType::Mean(2));
         let mut optimizer = SGD::new(parameters, 0.1);
 
         let data = vec![
@@ -63,12 +63,10 @@ mod tests {
                 // y.print();
                 optimizer.zero_grad().unwrap();
                 loss.backward();
-                let parameters2 = PrivateParameters::try_new(&model, 1000.0, 0.0, LossType::Mean(2)).unwrap();
-                parameters2.parameters[0].1.print();
                 optimizer.step().unwrap();
             }
         }
-        let w = &vs.trainable_variables()[0];
+        let w = &optimizer.parameters.into_inner()[0];
         assert!(l2_loss(w, &Tensor::of_slice::<f32>(&[2.])).unwrap().double_value(&[]) < 0.1);
     }
 
@@ -89,35 +87,36 @@ impl Optimizer for COptimizer {
 }
 
 pub trait Updatable {
-    fn update(&mut self, update_fn: impl FnMut(&mut Tensor, Tensor) -> Result<(), TchError>) -> Result<(), TchError>;
+    fn zero_grad(&mut self);
+    fn update(&mut self, update_fn: impl FnMut(&Tensor, Tensor) -> Result<Tensor, TchError>) -> Result<(), TchError>;
 }
 
 pub struct Parameters(Vec<Tensor>);
 
+impl From<VarStore> for Parameters {
+    fn from(vs: VarStore) -> Self {
+        Parameters(vs.trainable_variables())
+    }
+}
+
 impl Parameters {
-    pub fn try_new(module: &TrainableCModule) -> Result<Self, TchError> {
-        let err = Err(TchError::FileFormat(String::from("Invalid data, expected module to have a `trainable_parameters` function returning the dict of named trainable parameters.")));
-        Ok(Parameters(match module.method_is::<IValue>("trainable_parameters", &[])? {
-            IValue::GenericDict(v) => {
-                let mut res = Vec::with_capacity(v.len());
-                for (_, parameter) in v {
-                    match parameter {
-                        IValue::Tensor(t) => res.push(t),
-                        _ => return err,
-                    }
-                }
-                res
-            }
-            _ => return err,
-        }))
+    pub fn into_inner(mut self) -> Vec<Tensor> {
+        self.zero_grad();
+        self.0
     }
 }
 
 impl Updatable for Parameters {
-    fn update(&mut self, mut update_fn: impl FnMut(&mut Tensor, Tensor) -> Result<(), TchError>) -> Result<(), TchError> {
+    fn zero_grad(&mut self) {
+        for param in self.0.iter_mut() {
+            param.zero_grad();
+        }
+    }
+    fn update(&mut self, mut update_fn: impl FnMut(&Tensor, Tensor) -> Result<Tensor, TchError>) -> Result<(), TchError> {
         tch::no_grad(|| {
             for param in self.0.iter_mut() {
-                update_fn(param, param.f_grad()?)?;
+                let update = update_fn(param, param.f_grad()?)?;
+                let _ = param.f_sub_(&update)?;
             }
             Ok(())
         })
@@ -130,67 +129,56 @@ pub enum LossType {
 }
 
 pub struct PrivateParameters {
-    pub(crate) parameters: Vec<(Tensor, Tensor)>,
+    pub(crate) parameters: Vec<Tensor>,
     max_grad_norm: f64,
     noise_multiplier: f64,
     loss_type: LossType,
 }
 
 impl PrivateParameters {
-    pub fn try_new(module: &TrainableCModule, max_grad_norm: f64, noise_multiplier: f64, loss_type: LossType) -> Result<Self, TchError> {
-        let err = Err(TchError::FileFormat(String::from("Invalid data, expected module to have a `grad_sample_parameters` function returning the list of pairs of parameters and per-sample gradients.")));
-        let parameters = match module.method_is::<IValue>("grad_sample_parameters", &[])? {
-            IValue::GenericList(v) => {
-                let mut res = Vec::with_capacity(v.len());
-                for x in v {
-                    match x {
-                        IValue::Tuple(mut t) => {
-                            match t.remove(0) {
-                                IValue::Tensor(param) => {
-                                    match t.remove(0) {
-                                        IValue::Tensor(grad) => res.push((param, grad)),
-                                        _ => return err,
-                                    }
-                                },
-                                _ => return err,
-                            }
-                        }
-                        _ => return err,
-                    }
-                }
-                res
-            }
-            _ => return err,
-        };
-        Ok(PrivateParameters { parameters, max_grad_norm, noise_multiplier, loss_type })
+    pub fn new(vs: VarStore, max_grad_norm: f64, noise_multiplier: f64, loss_type: LossType) -> Self {
+        PrivateParameters { parameters: vs.trainable_variables(), max_grad_norm, noise_multiplier, loss_type }
+    }
+
+    // It is ok to retrieve the weights through
+    // this function as we erase the gradients prior
+    // to returning. Private data remain private.
+    pub fn into_inner(mut self) -> Vec<Tensor> {
+        self.zero_grad();
+        self.parameters
     }
 }
 
 impl Updatable for PrivateParameters {
-    fn update(&mut self, mut update_fn: impl FnMut(&mut Tensor, Tensor) -> Result<(), TchError>) -> Result<(), TchError> {
+    fn zero_grad(&mut self) {
+        for param in self.parameters.iter_mut() {
+            param.zero_grad();
+        }
+    }
+    fn update(&mut self, mut update_fn: impl FnMut(&Tensor, Tensor) -> Result<Tensor, TchError>) -> Result<(), TchError> {
         tch::no_grad(|| {
             let mut per_param_norms = Vec::with_capacity(self.parameters.len());
-            for (_, grad_sample) in self.parameters.iter() {
-                let dims: Vec<i64> = (1..grad_sample.dim()).map(|x| x as i64).collect();
-                per_param_norms.push(grad_sample.f_norm_scalaropt_dim(2, &dims, false)?);
+            for param in self.parameters.iter() {
+                let per_sample_grad = param.grad();
+                let dims: Vec<i64> = (1..per_sample_grad.dim()).map(|x| x as i64).collect();
+                per_param_norms.push(per_sample_grad.f_norm_scalaropt_dim(2, &dims, false)?);
             }
             let per_sample_norms = Tensor::f_stack(&per_param_norms, 1)?
                 .f_norm_scalaropt_dim(2, &[1], false)?;
             let max_grad_norm = Tensor::of_slice(&[self.max_grad_norm as f32]);
             let per_sample_clip_factor = max_grad_norm.f_div(&per_sample_norms.f_add_scalar(1e-6)?)?.f_clamp(0., 1.)?;
 
-            for (param, grad_sample) in self.parameters.iter_mut() {
-                grad_sample.print();
-                let grad = Tensor::f_einsum("i,i...", &[&per_sample_clip_factor, &grad_sample])?;
-                // grad.print();
-                let mut grad = grad.f_add(&generate_noise_like(&grad, self.noise_multiplier)?)?.f_view_as(&param)?;
-                // grad.print();
+            for param in self.parameters.iter_mut() {
+                let per_sample_grad = param.grad();
+                let mut update_size = per_sample_grad.size();
+                update_size.remove(0);
+                let grad = Tensor::f_einsum("i,i...", &[&per_sample_clip_factor, &per_sample_grad])?;
+                let mut grad = grad.f_add(&generate_noise_like(&grad, self.noise_multiplier)?)?.f_view(&update_size[..])?;
                 if let LossType::Mean(batch_size) = self.loss_type {
                     let _ = grad.f_div_scalar_(batch_size)?;
                 }
-                // grad.print();
-                update_fn(param, grad)?;
-                // param.print();
+                let update = update_fn(&param.i(0), grad)?;
+                let _ = param.i(0).f_sub_(&update)?;
             }
             Ok(())
         })
@@ -218,7 +206,7 @@ pub struct SGD<P> {
     momentum: f64,
     dampening: f64,
     nesterov: bool,
-    parameters: P,
+    pub parameters: P,
     statistics: Option<Tensor>,
 }
 
@@ -254,10 +242,8 @@ impl<P> SGD<P> {
 
 impl<P: Updatable> Optimizer for SGD<P> {
     fn zero_grad(&mut self) -> Result<(), TchError> {
-        self.parameters.update(|x, _| {
-            x.zero_grad();
-            Ok(())
-        })
+        self.parameters.zero_grad();
+        Ok(())
     }
 
     fn step(&mut self) -> Result<(), TchError> {
@@ -280,9 +266,8 @@ impl<P: Updatable> Optimizer for SGD<P> {
                     grad = (&self.statistics).as_ref().unwrap().f_detach_copy()?;
                 }
             }
-            // x = x - self.learning_rate * grad;
-            let _ = x.f_sub_(&grad.f_mul_scalar(self.learning_rate)?)?.f_set_requires_grad(true)?;
-            Ok(())
+            // update := self.learning_rate * grad;
+            grad.f_mul_scalar(self.learning_rate)
         })
     }
 }
