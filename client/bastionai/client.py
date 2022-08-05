c4231cfc3649c3251c7fa05503c11ac8c284fc06
kbamponsem <kwabenaamponsem@gmail.com>
2022-08-05 18:19:55 +0200

    Adds documentation to BastionAI client Python code

--- client/bastionai/client.py
+++ client/bastionai/client.py
@@ -18,37 +18,115 @@ class Client:
     stub: RemoteTorchStub
 
     def send_model(self, model: Module, description: str, secret: bytes) -> Reference:
+        """
+        send_model provides the endpoint to uploading Pytorch Modules to BastionAI.
+
+        Args:
+            model (Module): This is Pytorch's nn.Module.
+            description (str): A string description of the module being uploaded.
+            secret (bytes): User secret to secure module with.
+
+        Returns:
+            Reference: BastionAI reference object
+        """
         return self.stub.SendModel(serialize_model(model, description=description, secret=secret))
 
     def send_dataset(self, dataset: Dataset, description: str, secret: bytes) -> Reference:
+        """
+        send_data provides the endpoint for uploading Pytorch DataLoader to BastionAI.
+
+        Args:
+            model (Module): This is Pytorch's nn.Module.
+            description (str): A string description of the module being uploaded.
+            secret (bytes): User secret to secure module with.
+
+        Returns:
+            Reference: BastionAI reference object
+        """
         return self.stub.SendDataset(serialize_dataset(dataset, description=description, secret=secret))
 
     def fetch_model_weights(self, model: Module, ref: Reference) -> None:
+        """
+        Args:
+            model (Module): This is Pytorch's nn.Module corresponding to an uploaded module.
+            ref (Reference): BastionAI reference object corresponding to a module.
+
+        Returns:
+            None
+        """
         chunks = self.stub.FetchModule(ref)
         deserialize_weights_to_model(model, chunks)
 
     def fetch_dataset(self, ref: Reference) -> ArtifactDataset:
+        """
+        Args:
+            ref (Reference): BastionAI reference object corresponding to a dataset.
+
+        Returns:
+            ArtifactDataset: A wrapper to convert Tensors from BastionAI to Pytorch DataLoader
+        """
         return ArtifactDataset(self.stub.FetchDataset(ref))
 
     def get_available_models(self) -> List[Reference]:
+        """
+            Returns: 
+                List[Reference]: A list of BastionAI available model references.
+        """
         return self.stub.AvailableModels(Empty())
 
     def get_available_datasets(self) -> List[Reference]:
+        """
+            Returns: 
+                List[Reference]: A list of BastionAI available dataset references.
+        """
         return self.stub.AvailableDatasets(Empty())
 
     def get_available_devices(self) -> List[Devices]:
+        """
+            Returns: 
+                List[Reference]: A list of BastionAI available device references.
+        """
         return self.stub.AvailableDevices(Empty())
 
     def train(self, config: TrainConfig) -> None:
+        """
+        Trains model with `TrainConfig` on BastionAI server.
+
+        Args:
+            config (TrainConfig): Training configuration to pass to BastionAI.
+        Returns:
+            None
+        """
         self.stub.Train(config)
 
     def test(self, config: TestConfig) -> float:
+        """
+            
+        """
         return self.stub.Test(config)
 
     def delete_dataset(self, ref: Reference) -> None:
+        """
+            Delets dataset with reference from BastionAI server.
+
+            Args:
+                ref (Reference): BastionAI reference to dataset.
+
+            Returns:
+                None
+        """
         self.stub.DeleteDataset(ref)
 
     def delete_module(self, ref: Reference) -> None:
+        """
+            Delets model with reference from BastionAI server.
+
+            Args:
+                ref (Reference): BastionAI reference to dataset.
+
+            Returns:
+                None
+        """
         self.stub.DeleteModule(ref)
 
 
--- client/setup.py
+++ client/setup.py
@@ -1,6 +1,38 @@
+import os
 from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py
+import pkg_resources
+
+PROTO_FILES = ["remote_torch.proto"]
+PROTO_PATH = os.path.join(os.path.dirname(__file__), "proto")
+
+
+def generate_stub():
+    import grpc_tools.protoc
+
+    proto_include = pkg_resources.resource_filename("grpc_tools", "_proto")
+    for file in PROTO_FILES:
+        grpc_tools.protoc.main(
+            [
+                "grpc_tools.protoc",
+                "-I{}".format(proto_include),
+                "--proto_path={}".format(PROTO_PATH),
+                "--python_out=blindai/pb",
+                "--grpc_python_out=blindai/pb",
+                "{}".format(file),
+            ]
+        )
+
+
+class BuildPackage(build_py):
+    def run(self):
+        generate_stub()
+        super(BuildPackage, self).run()
+
 
 setup(
-    name = 'bastionai',
-    packages = find_packages(),
+    name='bastionai',
+    packages=find_packages(),
+    description="Client SDK for BastionAI Confidential AI Training.",
+    classifiers=["Programming Language :: Python :: 3"]
 )
