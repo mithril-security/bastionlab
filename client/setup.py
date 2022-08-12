895bb22f5429baa558e036ce828716c59d3d3b07
Lucas Bourtoule <lucas.bourtoule@mithrilsecurity.io>
2022-08-12 18:02:10 +0200

    make setuptools aware of build dependencies and stub generation

---
+++ client/setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import find_packages, setup
+import pkg_resources
+
+PROTO_FILES = ["remote_torch.proto"]
+PROTO_PATH = os.path.join(os.path.dirname(__file__), "../server/protos")
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+def generate_stub():
+    import grpc_tools.protoc
+
+    proto_include = pkg_resources.resource_filename("grpc_tools", "_proto")
+    for file in PROTO_FILES:
+        grpc_tools.protoc.main(
+            [
+                "grpc_tools.protoc",
+                "-I{}".format(proto_include),
+                "--proto_path={}".format(PROTO_PATH),
+                "--python_out=bastionai/pb",
+                "--grpc_python_out=bastionai/pb",
+                "{}".format(file),
+            ]
+        )
+
+# Create Protobuf files before packaging.
+generate_stub()
+
+setup(
+    name='bastionai',
+    version="0.1.0",
+    packages=find_packages(),
+    description="Client SDK for BastionAI Confidential AI Training.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kwabena Amponsem, Lucas Bourtoule',
+    author_email='kwabena.amponsem@mithrilsecurity.io, luacs.bourtoule@nithrilsecurity.io',
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=[
+        "grpcio==1.47.0",
+        "grpcio-tools==1.47.0",
+        "protobuf==3.20.1",
+        "six==1.16.0",
+        "torch==1.12.0",
+        "numpy==1.23.1",
+        "typing-extensions==4.3.0",
+        "tqdm==4.64.0"
+    ]
+)
