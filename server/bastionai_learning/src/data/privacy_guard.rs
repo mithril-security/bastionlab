9254c49c473b25ddeb106eec7f19893243baa2a2
Lucas Bourtoule <lucas.bourtoule@mithrilsecurity.io>
2022-09-01 16:53:37 +0200

    remove debug prints

--- server/bastionai_learning/src/data/privacy_guard.rs
+++ server/bastionai_learning/src/data/privacy_guard.rs
@@ -379,7 +379,6 @@ impl PrivacyGuard<Tensor> {
                     .value
                     .f_add(&generate_noise_like(&self.value, sigma as f64)?)?;
                 context.update_budget(budget);
-                println!("Guard sigma: {:?}", sigma);
                 Ok((res, sigma))
             }
         }
