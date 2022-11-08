f6a99d4259172fa6b29420425d08f2962070944a
kbamponsem <kwabenaamponsem@gmail.com>
2022-11-08 16:28:25 +0100

    Remove unnecessary comments

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 use polars::prelude::*;
+use serde_json;
 use std::{
     collections::HashMap,
     error::Error,
@@ -7,17 +8,13 @@ use std::{
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 use uuid::Uuid;
-use serde_json;
 
 pub mod grpc {
     tonic::include_proto!("bastionlab");
 }
 use grpc::{
-    Chunk,
-    Query,
-    ReferenceRequest,
-    ReferenceResponse,
-    bastion_lab_server::{BastionLab, BastionLabServer}
+    bastion_lab_server::{BastionLab, BastionLabServer},
+    Chunk, Query, ReferenceRequest, ReferenceResponse,
 };
 
 mod serialization;
@@ -42,7 +39,13 @@ impl BastionLabState {
 
     fn get_df(&self, identifier: &str) -> Result<DataFrame, Status> {
         let dfs = self.dataframes.read().unwrap();
-        Ok(dfs.get(identifier).ok_or(Status::not_found(format!("Could not find dataframe: identifier={}", identifier)))?.clone())
+        Ok(dfs
+            .get(identifier)
+            .ok_or(Status::not_found(format!(
+                "Could not find dataframe: identifier={}",
+                identifier
+            )))?
+            .clone())
     }
 
     // fn get_dfs(&self, identifiers: &[String]) -> Result<VecDeque<DataFrame>, Status> {
@@ -66,14 +69,29 @@ impl BastionLabState {
 impl BastionLab for BastionLabState {
     type FetchDataFrameStream = ReceiverStream<Result<Chunk, Status>>;
 
-    async fn run_query(&self, request: Request<Query>) -> Result<Response<ReferenceResponse>, Status> {
+    async fn run_query(
+        &self,
+        request: Request<Query>,
+    ) -> Result<Response<ReferenceResponse>, Status> {
         // let input_dfs = self.get_dfs(&request.get_ref().identifiers)?;
         println!("{:?}", request);
         println!("{}", &request.get_ref().composite_plan);
-        let composite_plan: CompositePlan = serde_json::from_str(&request.get_ref().composite_plan).map_err(|e| Status::invalid_argument(format!("Could not deserialize composite plan: {}{}", e, &request.get_ref().composite_plan)))?;
+        let composite_plan: CompositePlan = serde_json::from_str(&request.get_ref().composite_plan)
+            .map_err(|e| {
+                Status::invalid_argument(format!(
+                    "Could not deserialize composite plan: {}{}",
+                    e,
+                    &request.get_ref().composite_plan
+                ))
+            })?;
         let res = composite_plan.run(self)?;
 
-        let header = serde_json::to_string(&res.schema()).map_err(|e| Status::internal(format!("Could not serialize result data frame header: {}", e)))?;
+        let header = serde_json::to_string(&res.schema()).map_err(|e| {
+            Status::internal(format!(
+                "Could not serialize result data frame header: {}",
+                e
+            ))
+        })?;
         let identifier = self.insert_df(res);
         Ok(Response::new(ReferenceResponse { identifier, header }))
     }
@@ -83,13 +101,11 @@ impl BastionLab for BastionLabState {
         request: Request<Streaming<Chunk>>,
     ) -> Result<Response<ReferenceResponse>, Status> {
         let df = df_from_stream(request.into_inner()).await?;
-        
-        let header = serde_json::to_string(&df.schema()).map_err(|e| Status::internal(format!("Could not serialize header: {}", e)))?;
+
+        let header = serde_json::to_string(&df.schema())
+            .map_err(|e| Status::internal(format!("Could not serialize header: {}", e)))?;
         let identifier = self.insert_df(df);
-        Ok(Response::new(ReferenceResponse {
-            identifier,
-            header,
-        }))
+        Ok(Response::new(ReferenceResponse { identifier, header }))
     }
 
     async fn fetch_data_frame(
@@ -106,7 +122,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let state = BastionLabState::new();
     let addr = "[::1]:50056".parse()?;
     println!("BastionLab server running...");
-    // println!("{:?}", serde_json::from_str::<CompositePlan>("[{\"EntryPointPlanSegment\":\"1da61d9a-c8a8-4e8e-baec-b132db9009d9\"},{\"EntryPointPlanSegment\":\"1da61d9a-c8a8-4e8e-baec-b132db9009d9\"}]").unwrap());
     Server::builder()
         .add_service(BastionLabServer::new(state))
         .serve(addr)
