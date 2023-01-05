4104aa9625cbcf952011abebfb77d630d1b15874
Théo Le Bever <theolebever@gmail.com>
2023-01-05 11:06:56 +0100

    Try with markdownify

---
+++ docs/docs/gen_ref_pages.py
@@ -0,0 +1,13 @@
+import markdownify
+import os
+
+PATH = "./docs/resources/bastionlab/"
+
+# create html
+for files in os.listdir(PATH):
+    if files.endswith(".html"):
+        with open(PATH + files, "r") as f:
+            html = f.read()
+            h = markdownify.markdownify(html)
+            with open(PATH + files.replace(".html", ".md"), "w", encoding="utf-8") as f:
+                f.write(h)
