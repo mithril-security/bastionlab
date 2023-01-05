5505e65deb7437696066266171aa580b33bb49e3
Théo Le Bever <theolebever@gmail.com>
2023-01-05 15:18:20 +0100

    Adding missing files + now delete 31 first lines of the markdown converted files, just style element from html

--- docs/docs/gen_ref_pages.py
+++ docs/docs/gen_ref_pages.py
@@ -1,13 +1,19 @@
 import markdownify
 import os
+from glob import iglob
 
-PATH = "./docs/docs/resources/bastionlab/"
+PATH = "./docs/docs/resources/bastionlab/**/*"
 
-# create html
-for files in os.listdir(PATH):
-    if files.endswith(".html"):
-        with open(PATH + files, "r") as f:
+# Create file list
+file_list = [f for f in iglob(PATH, recursive=True) if os.path.isfile(f)]
+
+for file in file_list:
+    if file.endswith(".html"):
+        with open(file, "r", encoding="utf-8") as f:
             html = f.read()
             h = markdownify.markdownify(html)
-            with open(PATH + files.replace(".html", ".md"), "w", encoding="utf-8") as f:
+            # delete the first 31 lines of the file
+            split = h.splitlines(True)[31:]
+            h = "".join(split)
+            with open(file.replace(".html", ".md"), "w", encoding="utf-8") as f:
                 f.write(h)
