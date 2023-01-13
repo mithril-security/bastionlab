94822a223a09150c7e35d9e373bdba9499a8d69e
Théo Le Bever <theolebever@gmail.com>
2023-01-13 14:57:07 +0100

    First attempt at expanding only a specific level of the nav bar for documentatiion

--- docs/javascripts/navbar.js
+++ docs/javascripts/navbar.js
@@ -1,6 +1,32 @@
-$(document).ready(function(){
-    $('.md-nav[data-md-level="3"]').hide(); // hide all elements with class "md-nav" and data-md-level="3"
-    $('.md-nav__item').click(function(){
-        $(this).find('.md-nav[data-md-level="4"]').toggle(); // toggle all elements with class "md-nav" and data-md-level="4" inside the clicked element
+// Search for every .md-nav__link element
+// For every .md-nav__link element, verify if it contains an .md-nav[data-md-level="2"] element
+// If it contains an .md-nav[data-md-level="2"] element, display it and rotate the arrow by 90 degrees
+// If it doesn't contain an .md-nav[data-md-level="2"] element, do not touch it and do not rotate the arrow
+
+$(document).ready(function () {
+    $('.md-nav__link').each(function () {
+        var subMenu = $(this).parent().find('.md-nav[data-md-level="2"]');
+        if (subMenu.length) {
+            subMenu.show();
+            $(this).find('.md-nav__icon').css('transform', 'rotate(90deg)');
+            // Add a click event to the .md-nav__link element
+            $(this).click(function () {
+                // Find the sub-menu element
+                var subMenu = $(this).parent().find('.md-nav[data-md-level="2"]');
+                // If the sub-menu is hidden, show it
+                if (subMenu.is(':hidden')) {
+                    subMenu.show();
+                    // Rotate the arrow down
+                    // $(this).find('.md-nav__icon').css('transform', 'rotate(90deg)');
+                }
+                // If the sub-menu is visible, hide it
+                else {
+                    subMenu.hide();
+                    // Rotate the arrow by -90 degrees
+                    // $(this).find('.md-nav__icon').css('transform', 'rotate(-90deg)');
+                }
+            }
+            );
+        }
     });
 });
