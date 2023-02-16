f4e17c157015fca5fb471f4caf60aa71a7ac4efe
AndreIglesias <AndreIglesias@users.noreply.github.com>
2023-02-16 10:55:16 +0000

    Auto-sync versions
    Files changed:
    M	.env.vars
    M	client/src/bastionlab/version.py

--- client/src/bastionlab/version.py
+++ client/src/bastionlab/version.py
@@ -4,4 +4,21 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
-__all__ = []
+__all__ = [
+    "__torch_version__",
+    "__torch_url__",
+    "__torch_cxx11_url__",
+    "__version__",
+]
+__torch_version__ = "1.13.1"
+__torch_url__ = (
+    "https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-"
+    + __torch_version__
+    + "%2Bcpu.zip"
+)
+__torch_cxx11_url__ = (
+    "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-"
+    + __torch_version__
+    + "%2Bcpu.zip"
+)
+__version__ = "0.3.7"
